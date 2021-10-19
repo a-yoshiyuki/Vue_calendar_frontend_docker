3a88fe8b48b1215a79cae20b4cd4870c8d0df8c9
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-10-19 22:28:27 +0900

    [Add]予定一覧表示機能　実装

--- app/frontend/src/components/pages/Calendar.vue
+++ app/frontend/src/components/pages/Calendar.vue
@@ -21,10 +21,14 @@
           :events="events"
           @change="fetchEvents"
           locale="ja-jp"
-          :day-format="timestamp => new Date(timestamp.date).getDate()"
-          :month-format="timestamp => new Date(timestamp.date).getMonth() + 1 + ' /'"
+          :day-format="(timestamp) => new Date(timestamp.date).getDate()"
+          :month-format="
+            (timestamp) => new Date(timestamp.date).getMonth() + 1 + ' /'
+          "
           @click:event="showEvent"
           @click:day="initEvent"
+          @click:date="showDayEvents"
+          @click:more="showDayEvents"
         ></v-calendar>
       </v-sheet>
     </v-sheet>
@@ -32,6 +36,14 @@
     <v-dialog :value="event !== null" @click:outside="closeDialog" width="600">
       <EventDetailDialog v-if="event !== null && !isEditMode" />
       <EventFormDialog v-if="event !== null && isEditMode" />
+    </v-dialog>
+
+    <v-dialog
+      :value="clickedDate !== null"
+      @click:outside="closeDialog"
+      width="600"
+    >
+      <DayEventList />
     </v-dialog>
   </div>
 </template>
@@ -41,7 +53,8 @@ import { format } from "date-fns";
 import { mapGetters, mapActions } from "vuex";
 import EventDetailDialog from "../events/EventDetailDialog";
 import EventFormDialog from "../events/EventFormDialog";
-import CalendarList from '../calendars/CalendarList';
+import DayEventList from "../events/DayEventList";
+import CalendarList from "../calendars/CalendarList";
 import { getDefaultStartAndEnd } from "../../functions/datetime";
 
 export default {
@@ -50,18 +63,24 @@ export default {
     EventDetailDialog,
     EventFormDialog,
     CalendarList,
+    DayEventList,
   },
   data: () => ({
     value: format(new Date(), "yyyy/MM/dd"),
   }),
   computed: {
-    ...mapGetters("events", ["events", "event", "isEditMode"]),
+    ...mapGetters("events", ["events", "event", "isEditMode", "clickedDate"]),
     title() {
       return format(new Date(this.value), "yyyy年 M月");
     },
   },
   methods: {
-    ...mapActions("events", ["fetchEvents", "setEvent", "setEditMode"]),
+    ...mapActions("events", [
+      "fetchEvents",
+      "setEvent",
+      "setEditMode",
+      "setClickedDate",
+    ]),
     setToday() {
       this.value = format(new Date(), "yyyy/MM/dd");
     },
@@ -72,12 +91,20 @@ export default {
     closeDialog() {
       this.setEvent(null);
       this.setEditMode(false);
+      this.setClickedDate(null);
     },
     initEvent({ date }) {
+      if (this.clickedDate !== null) {
+        return;
+      }
       date = date.replace(/-/g, "/");
       const [start, end] = getDefaultStartAndEnd(date);
       this.setEvent({ name: "", start, end, timed: true });
       this.setEditMode(true);
+    },
+    showDayEvents({ date }) {
+      date = date.replace(/-/g, "/");
+      this.setClickedDate(date);
     },
   },
 };
